09cc5e395e927a1c347a7e503a99279d0a9322bd
Luigi <luigi.fiori.lf@gmail.com>
2024-02-16 06:23:04 +0000

    add terraform.tfvars file to store variables values

--- terraform/main.tf
+++ terraform/main.tf
@@ -378,3 +378,15 @@ resource "aws_instance" "web_server_2" {
   subnet_id     = aws_subnet.public_subnets["public_subnet_2"].id
   tags          = local.common_tags
 }
+
+resource "aws_subnet" "variables-subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.variables_sub_cidr
+  availability_zone       = var.variables_sub_az
+  map_public_ip_on_launch = var.variables_sub_auto_ip
+
+  tags = {
+    Name      = "sub-variables-${var.variables_sub_az}"
+    Terraform = "true"
+  }
+}
