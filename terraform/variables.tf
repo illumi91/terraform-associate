3b116eb6a9880efa24f994105d23a15e840f62ee
Luigi <luigi.fiori.lf@gmail.com>
2024-03-04 08:44:46 +0000

    test 1

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -59,17 +59,6 @@ variable "phone_number" {
   default   = "867-5309"
 }
 
-variable "us-east-1-azs" {
-  type = list(string)
-  default = [
-    "us-east-1a",
-    "us-east-1b",
-    "us-east-1c",
-    "us-east-1d",
-    "us-east-1e"
-  ]
-}
-
 variable "ip" {
   type = map(string)
   default = {
