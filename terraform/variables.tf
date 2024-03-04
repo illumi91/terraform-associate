457d88c93dc1348f3118b6506fe1d1facf6d0db2
Luigi <luigi.fiori.lf@gmail.com>
2024-03-04 08:46:28 +0000

    test2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -78,19 +78,4 @@ variable "ip" {
   }
 }
 
-variable "env" {
-  type = map(any)
-  default = {
-    prod = {
-      ip = "10.0.150.0/24"
-      az = "us-east-1a"
-    }
-    dev = {
-      ip = "10.0.250.0/24"
-      az = "us-east-1e"
-    }
-  }
-}
-
-
 
